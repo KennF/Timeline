6ed710d76d1e7e46c2ef6b9182ecd6e88e964eec
KennF <cactus.fxy@gmail.com>
2013-07-10 19:12:44 +0800

    add init blog structure

---
+++ blog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config.from_object('config.DevConfig')
+
+db = MongoEngine(app)
+
+def register_blueprint(app):
+	from blog.views import posts
+	app.register_blueprint(posts)
+
+register_blueprint(app)
+
+if __name__ == '__main__':
+    app.run()
+
